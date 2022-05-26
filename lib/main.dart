2941331385962b758e2965a87a7c42da727d8373
Fernando Silva <fernnlcs@gmail.com>
2022-05-26 16:29:04 -0300

    Created first container

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.deepOrange,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            margin: EdgeInsets.all(20),
+            height: 100,
+            width: 100,
+            color: Colors.white,
+            child: Text('Hello, guys!'),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
